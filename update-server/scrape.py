30be92b462f24a8074bb6fb00d51ba914016cde9
aarkan <johanwiren1@gmail.com>
2022-11-05 15:29:54 +0100

    Handle scrape error

--- update-server/scrape.py
+++ update-server/scrape.py
@@ -6,14 +6,23 @@ import re
 
 url = 'https://www.minecraft.net/en-us/download/server/bedrock'
 
-headers = { 
+headers = {
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
 }
 
-res = requests.get(url, headers=headers)
+try:
+    res = requests.get(url, headers=headers, allow_redirects=False, timeout=10)
+except Exception as e:
+    print(e)
+    with open('update-server/scrape.log', 'w') as log:
+        log.write('ERROR: ' + e)
+
 html = BS(res.text, features="html.parser")
 link = html.find(class_='downloadlink')['href']
 
 bedrock_server_version = re.search(r'\d+\.\d+\.\d+\.\d+', link).group()
+
+with open('update-server/scrape.log', 'w') as log:
+    log.write('bedrock version: ' + bedrock_server_version)
 
 print(bedrock_server_version)
